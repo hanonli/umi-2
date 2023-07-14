1182c86886c98a8057a735ac5116fd5677c86de3
Somruethai Onamphai <Somruethai@got.co.th>
2023-07-14 14:07:24 +0700

    add trace config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,41 @@
 import yayJpg from '../assets/yay.jpg';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import { ConsoleSpanExporter, SimpleSpanProcessor, TracerConfig, WebTracerProvider, BatchSpanProcessor } from '@opentelemetry/sdk-trace-web';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { Resource } from '@opentelemetry/resources';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+
+const collectorExporter = new OTLPTraceExporter({
+  url: "http://172.31.6.206:4330/v1/traces",
+  headers: {}
+});
+
+const providerConfig: TracerConfig = {
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'umi-2', // or call APP_NAME by calling process.env.APP_NAME 
+  }),
+};
+
+const provider = new WebTracerProvider(providerConfig);
+
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+provider.addSpanProcessor(new BatchSpanProcessor(collectorExporter));
+
+provider.register({
+  contextManager: new ZoneContextManager(),
+});
+
+registerInstrumentations({
+  instrumentations: [
+    getWebAutoInstrumentations({
+      '@opentelemetry/instrumentation-fetch': {
+	
+      },
+    }),
+  ],
+});
 
 export default function HomePage() {
   return (
