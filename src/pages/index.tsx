51237826deea128bed73870f6df0391fc731301a
Somruethai Onamphai <Somruethai@got.co.th>
2023-07-17 15:06:56 +0700

    test-3-06

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,58 @@
 import yayJpg from '../assets/yay.jpg';
-import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
-import { ConsoleSpanExporter, SimpleSpanProcessor, TracerConfig, WebTracerProvider, BatchSpanProcessor } from '@opentelemetry/sdk-trace-web';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
-import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-import { Resource } from '@opentelemetry/resources';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
-const collectorExporter = new OTLPTraceExporter({
-  url: "http://172.31.12.83:4318/v1/traces",
-  // url: "http://10.10.70.112:4330/v1/traces",
-  headers: {
-    'Access-Control-Allow-Origin': '*'
-  }
+const exporter = new OTLPTraceExporter({
+//  url: 'http://172.31.12.83:4318/v1/traces',
+ url: 'http://10.10.70.112:4330/v1/traces',
+ headers: {}
 });
+const provider = new WebTracerProvider({
+ resource: new Resource({
 
-const providerConfig: TracerConfig = {
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'umi-2', // or call APP_NAME by calling process.env.APP_NAME 
-  }),
-};
-
-const provider = new WebTracerProvider(providerConfig);
-
-provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-provider.addSpanProcessor(new BatchSpanProcessor(collectorExporter));
-
+   [SemanticResourceAttributes.SERVICE_NAME]: 'umi-2',
+ }),
+});
+provider.addSpanProcessor(new BatchSpanProcessor(exporter));
 provider.register({
-  contextManager: new ZoneContextManager(),
+ contextManager: new ZoneContextManager()
 });
 
 registerInstrumentations({
-  instrumentations: [
-    getWebAutoInstrumentations({
-      '@opentelemetry/instrumentation-fetch': {
-	
-      },
-    }),
-  ],
+ instrumentations: [
+   getWebAutoInstrumentations({
+     // load custom configuration for xml-http-request instrumentation
+     '@opentelemetry/instrumentation-xml-http-request': {
+       propagateTraceHeaderCorsUrls: [
+           /.+/g,
+         ],
+     },
+     // load custom configuration for fetch instrumentation
+     '@opentelemetry/instrumentation-fetch': {
+       propagateTraceHeaderCorsUrls: [
+           /.+/g,
+         ],
+     },
+   }),
+ ],
 });
 
 export default function HomePage() {
   return (
-    <div>
-      <h2>Yay! Welcome to umi! umi-1:47</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
-      <p>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
-    </div>
+      <div>
+        <h2>Yay! Welcome to umi! umi-1:47</h2>
+        <p>
+          <img src={yayJpg} width="388" />
+        </p>
+        <p>
+          To get started, edit <code>pages/index.tsx</code> and save to reload.
+        </p>
+      </div>
   );
 }
