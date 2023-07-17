395d859222770702629580db4cf3aa984a3d8a09
Somruethai Onamphai <Somruethai@got.co.th>
2023-07-17 13:48:50 +0700

    test-1-48

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,13 @@ import yayJpg from '../assets/yay.jpg';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
 import { ConsoleSpanExporter, SimpleSpanProcessor, TracerConfig, WebTracerProvider, BatchSpanProcessor } from '@opentelemetry/sdk-trace-web';
-// import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { Resource } from '@opentelemetry/resources';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 
 const collectorExporter = new OTLPTraceExporter({
-  url: "http://172.31.6.206:4330/v1/traces",
+  url: "http://172.31.12.83:4318/v1/traces",
   // url: "http://10.10.70.112:4330/v1/traces",
   headers: {
     'Access-Control-Allow-Origin': '*'
@@ -27,7 +27,7 @@ provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 provider.addSpanProcessor(new BatchSpanProcessor(collectorExporter));
 
 provider.register({
-  // contextManager: new ZoneContextManager(),
+  contextManager: new ZoneContextManager(),
 });
 
 registerInstrumentations({
@@ -43,7 +43,7 @@ registerInstrumentations({
 export default function HomePage() {
   return (
     <div>
-      <h2>Yay! Welcome to umi! umi-12:48</h2>
+      <h2>Yay! Welcome to umi! umi-1:47</h2>
       <p>
         <img src={yayJpg} width="388" />
       </p>
