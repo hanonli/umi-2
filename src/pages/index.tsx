e1100feca5e0b4ea852c4007ee38ebf56d5dd90f
Somruethai Onamphai <Somruethai@got.co.th>
2023-07-19 14:36:16 +0700

    add tracing.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,18 @@
 import yayJpg from '../assets/yay.jpg';
-import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
-import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
-import { registerInstrumentations } from '@opentelemetry/instrumentation';
-import { ZoneContextManager } from '@opentelemetry/context-zone';
-const { Resource } = require('@opentelemetry/resources');
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
-
-const exporter = new OTLPTraceExporter({
- url: 'http://3.106.54.87:4318/v1/traces',
-//  url: 'http://10.10.70.112:4330/v1/traces',
- headers: {}
-});
-const provider = new WebTracerProvider({
- resource: new Resource({
-   [SemanticResourceAttributes.SERVICE_NAME]: 'umi-2', // can use 'process.env.APP_NAME' instead of this
- }),
-});
-provider.addSpanProcessor(new BatchSpanProcessor(exporter));
-provider.register({
- contextManager: new ZoneContextManager()
-});
-
-registerInstrumentations({
- instrumentations: [
-   getWebAutoInstrumentations({
-     // load custom configuration for xml-http-request instrumentation
-     '@opentelemetry/instrumentation-xml-http-request': {
-       propagateTraceHeaderCorsUrls: [
-           /.+/g,
-         ],
-     },
-     // load custom configuration for fetch instrumentation
-     '@opentelemetry/instrumentation-fetch': {
-       propagateTraceHeaderCorsUrls: [
-           /.+/g,
-         ],
-     },
-   }),
- ],
-});
+import TraceProvider from './tracing.js';
 
 export default function HomePage() {
   return (
-      <div>
-        <h2>Yay! Welcome to umi! umi-1:47</h2>
-        <p>
-          <img src={yayJpg} width="388" />
-        </p>
-        <p>
-          To get started, edit <code>pages/index.tsx</code> and save to reload.
-        </p>
-      </div>
+      <TraceProvider>
+        <div>
+          <h2>Yay! Welcome to umi! umi-1:47</h2>
+          <p>
+            <img src={yayJpg} width="388" />
+          </p>
+          <p>
+            To get started, edit <code>pages/index.tsx</code> and save to reload.
+          </p>
+        </div>
+      </TraceProvider>
   );
 }
---
+++ src/pages/tracing.js
@@ -0,0 +1,50 @@
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+
+const exporter = new OTLPTraceExporter({
+ url: 'http://3.106.54.87:4318/v1/traces',
+//  url: 'http://10.10.70.112:4330/v1/traces',
+ headers: {}
+});
+const provider = new WebTracerProvider({
+ resource: new Resource({
+   [SemanticResourceAttributes.SERVICE_NAME]: 'umi-2', // can use 'process.env.APP_NAME' instead of this
+ }),
+});
+provider.addSpanProcessor(new BatchSpanProcessor(exporter));
+provider.register({
+ contextManager: new ZoneContextManager()
+});
+
+registerInstrumentations({
+ instrumentations: [
+   getWebAutoInstrumentations({
+     // load custom configuration for xml-http-request instrumentation
+     '@opentelemetry/instrumentation-xml-http-request': {
+       propagateTraceHeaderCorsUrls: [
+           /.+/g,
+         ],
+     },
+     // load custom configuration for fetch instrumentation
+     '@opentelemetry/instrumentation-fetch': {
+       propagateTraceHeaderCorsUrls: [
+           /.+/g,
+         ],
+     },
+   }),
+ ],
+});
+
+export default function TraceProvider ({ children }) {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
